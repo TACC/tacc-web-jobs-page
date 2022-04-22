5f26247284de69c2b74b95c4c427e31a0e944a8e
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-04-22 12:10:45 -0500

    fix(output): more consistent with CMS markup

--- bin/markdown-to-html.js
+++ bin/markdown-to-html.js
@@ -1,7 +1,10 @@
+// NOTE: All markdown parsing is commented out;
+//       unnecessary to parse cuz description is only ever one <p> of plain text
+
 // Include dependencies
 const fs = require('fs');
-const remark = require('remark');
-const remarkHtml = require('remark-html')
+// const remark = require('remark');
+// const remarkHtml = require('remark-html');
 
 // Arguments
 // TODO: Define these values externally
@@ -13,56 +16,56 @@ const __LIST_TYPE__ = 'array';
 const json = fs.readFileSync('./assets/_data.json');
 const data = JSON.parse(json);
 
-/** Convert content from Markdown to HTML */
-function convert(markdown) {
-  const markup = remark().use(remarkHtml, {
-    sanitize: true
-  }).processSync(markdown).toString();
+// /** Convert content from Markdown to HTML */
+// function convert(markdown) {
+//   const markup = remark().use(remarkHtml, {
+//     sanitize: true
+//   }).processSync(markdown).toString();
 
-  return markup;
-}
+//   return markup;
+// }
 
-/** Convert all relevant content, within given entries, from Markdown to HTML */
-function convertAll(data) {
-  let entryName, entry, markdown, markup;
-  const property = __MARKDOWN_PROP_NAME__;
-  const entries = data[__DATA_PROP_NAME__];
-  const isIterable = (typeof entries === 'object' && entries !== null);
+// /** Convert all relevant content, within given entries, from Markdown to HTML */
+// function convertAll(data) {
+//   let entryName, entry, markdown, markup;
+//   const property = __MARKDOWN_PROP_NAME__;
+//   const entries = data[__DATA_PROP_NAME__];
+//   const isIterable = (typeof entries === 'object' && entries !== null);
 
-  // Fail early
-  if ( ! isIterable) {
-    throw new Error(`The provided data (of type ${(typeof entries)}) is not iterable`);
-  }
+//   // Fail early
+//   if ( ! isIterable) {
+//     throw new Error(`The provided data (of type ${(typeof entries)}) is not iterable`);
+//   }
 
-  switch (__LIST_TYPE__) {
-    case 'array':
-      entries.forEach( entry => {
-        markdown = entry[property];
-        markup = convert(markdown);
+//   switch (__LIST_TYPE__) {
+//     case 'array':
+//       entries.forEach( entry => {
+//         markdown = entry[property];
+//         markup = convert(markdown);
 
-        entry[property] = markup;
-        // console.debug({markup});
-      });
-      break;
+//         entry[property] = markup;
+//         // console.debug({markup});
+//       });
+//       break;
 
-    case 'object':
-      for (entryName in entries) {
-        entry = entries[entryName];
-        markdown = entry[property];
-        markup = convert(markdown);
+//     case 'object':
+//       for (entryName in entries) {
+//         entry = entries[entryName];
+//         markdown = entry[property];
+//         markup = convert(markdown);
 
-        entry[property] = markup;
-        // console.debug({markup});
-      }
-      break;
+//         entry[property] = markup;
+//         // console.debug({markup});
+//       }
+//       break;
 
-    default:
-      throw new Error('Unrecognized `__LIST_TYPE__`:' + __LIST_TYPE__);
-  }
-}
+//     default:
+//       throw new Error('Unrecognized `__LIST_TYPE__`:' + __LIST_TYPE__);
+//   }
+// }
 
 // Manipulate data
-convertAll(data);
+// convertAll(data);
 // console.debug(JSON.stringify(data));
 
 module.exports = data;
